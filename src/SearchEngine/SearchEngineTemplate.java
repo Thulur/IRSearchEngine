60badb70a6c962874d80d35a22a7e7bbd60ee3f2
Sebastian Koall <sebastian.koall@student.hpi.uni-potsdam.de>
2015-10-20 10:01:39 +0200

    Add method to stem text phrases

--- src/SearchEngine/SearchEngineTemplate.java
+++ src/SearchEngine/SearchEngineTemplate.java
@@ -18,7 +18,16 @@ package SearchEngine;
  * Keep in mind to include your implementation decisions also in the pdf file of each assignment
  */
 
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
 
 
 public class SearchEngineTemplate extends SearchEngine { // Replace 'Template' with your search engine's name, i.e. SearchEngineMyTeamName
@@ -31,6 +40,27 @@ public class SearchEngineTemplate extends SearchEngine { // Replace 'Template' w
 
     @Override
     void index(String directory) {
+        stem("enter some text here");
+    }
+
+    private List<String> stem(String text) {
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props, false);
+        Annotation document = pipeline.process(text);
+        LinkedList<String> lemmas = new LinkedList<String>();
+
+        for(CoreMap sentence: document.get(CoreAnnotations.SentencesAnnotation.class))
+        {
+            for(CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class))
+            {
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
+                lemmas.add(lemma);
+            }
+        }
+
+        return lemmas;
     }
 
     @Override
