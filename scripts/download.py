61feb98e01b2c0c63f9c4b36a2b4351c254243ef
Sebastian Koall <sebastian.koall@student.hpi.uni-potsdam.de>
2015-11-01 15:09:13 +0100

    Implementing downloading of all ipg files from source

---
+++ scripts/download.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import urllib
+
+from urllib.request import urlopen
+
+url = "http://www.google.com/googlebooks/uspto-patents-grants-text.html"
+page_content = urlopen(url).read().decode("utf-8")
+
+links = re.findall('http://storage.googleapis.com/patents/grant_full_text/20\d{2}/ipg\d{6}.zip', page_content)
+for link in links:
+    f = urlopen(link)
+    file_count = 0
+
+    with open("../data/ipg/" + link[-13:], "wb") as local_file:
+        local_file.write(f.read())
+        file_count += 1
+        print("Downloaded " + str(file_count) + "/" + str(len(links)))
+        
