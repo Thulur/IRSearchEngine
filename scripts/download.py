f23fb2e63f7e971b7bc07ab36d00e9ce45fbeaf9
Sebastian Koall <koall.sebastian@gmail.com>
2015-11-02 09:34:27 +0100

    Update scripts
    - preprocessing works fine now
    - fix filecounter in the download script

--- scripts/download.py
+++ scripts/download.py
@@ -11,9 +11,9 @@ url = "http://www.google.com/googlebooks/uspto-patents-grants-text.html"
 page_content = urlopen(url).read().decode("utf-8")
 
 links = re.findall('http://storage.googleapis.com/patents/grant_full_text/20\d{2}/ipg\d{6}.zip', page_content)
+file_count = 0
 for link in links:
     f = urlopen(link)
-    file_count = 0
 
     with open("../data/ipgzip/" + link[-13:], "wb") as local_file:
         local_file.write(f.read())
