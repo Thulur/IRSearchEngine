9b1906b22b7095334c38515f1cd847012662fbac
Sebastian Koall <sebastian.koall@student.hpi.uni-potsdam.de>
2015-11-24 19:22:55 +0100

    Add missing Preloader class

---
+++ src/SearchEngine/data/Preloader.java
@@ -0,0 +1,40 @@
+package SearchEngine.data;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * Created by sebastian on 24.11.2015.
+ */
+public class Preloader implements  Runnable {
+    int bufferSize = 2 << 16;
+    private boolean loadingNeeded = true;
+    private byte[] preloadBuffer = new byte[bufferSize];
+    RandomAccessFile file;
+
+    public Preloader(RandomAccessFile file) throws IOException {
+        this.file = file;
+    }
+
+    @Override
+    public void run() {
+        if (loadingNeeded) {
+            try {
+                preloadBuffer = new byte[bufferSize];
+                file.read(preloadBuffer);
+                loadingNeeded = false;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public byte[] getPreloadBuffer() {
+        loadingNeeded = true;
+        return preloadBuffer;
+    }
+
+    public boolean isLoadingNeeded() {
+        return loadingNeeded;
+    }
+}
