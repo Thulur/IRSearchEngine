d8f94e3669b81e00896977f5034ebf442a3b9a2c
Sebastian Koall <sebastian.koall@student.hpi.uni-potsdam.de>
2015-12-01 11:39:51 +0100

    Refactor PRFSearch

--- src/SearchEngine/data/Configuration.java
+++ src/SearchEngine/data/Configuration.java
@@ -6,5 +6,6 @@ package SearchEngine.data;
 public class Configuration {
     public static boolean FILTER_STOPWORDS_IN_PHRASES = true;
     public static boolean ENABLE_SPELLING_CORRECTION = true;
+    public static int PRF_WORD_COUNT = 3;
     public static boolean EXPORT_FORMAT = false;
 }
