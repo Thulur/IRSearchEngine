32a6f46a05223ac24e3017514aa3dbec0a3779ec
mr-flannery <dennis_hempfing@gmx.de>
2016-02-25 19:38:58 +0100

    Add parsing of hashtags to determine prf queries

--- src/SearchEngine/search/SearchFactory.java
+++ src/SearchEngine/search/SearchFactory.java
@@ -4,6 +4,8 @@ import SearchEngine.index.ContentIndex;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by sebastian on 10.11.2015.
@@ -29,10 +31,17 @@ public class SearchFactory {
 
         Search search;
 
+        int prf = 0;
+        Pattern prfPattern = Pattern.compile(".*[#]\\d+");
+        Matcher prfMatcher = prfPattern.matcher(query);
+        if (prfMatcher.matches()) {
+            prf = Integer.parseInt(query.substring(query.indexOf("#") + 1, query.length()));
+        }
+
         if (queryIsBoolean) {
             search = new BooleanSearch();
-        //} else if (prf > 0) {
-        //    search = new PRFSearch();
+        } else if (prf > 0) {
+            search = new PRFSearch();
         } else {
             search = new VectorSpaceSearch();
         }
